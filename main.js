831c691d7793c0c9b478544e3a3817ed7c5a31d2
Tian-W001 <wt1435831399@gmail.com>
2023-09-13 22:02:59 +1000

    implement a FPS camera controller for testing

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
 
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-
-
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls.js";
 
 
 function createCamera() {
@@ -17,6 +16,97 @@ function createCamera() {
     return camera;
 }
 
+//FPS control for testing
+function createControls(camera, canvas) {
+
+    const controls = new PointerLockControls(camera, canvas);
+
+    const moveSpeed = 3;
+    let forward = false;
+    let backward = false;
+    let left = false;
+    let right = false;
+    let up = false;
+    let down = false;
+
+    controls.update = (delta) => {
+        if (forward)    {
+            controls.moveForward(moveSpeed * delta);
+        }
+        if (backward)   {
+            controls.moveForward(-moveSpeed * delta);
+        }
+        if (left)       {
+            controls.moveRight(-moveSpeed * delta);
+        }
+        if (right)      {
+            controls.moveRight(moveSpeed * delta);
+        }
+        if (up)         {
+            controls.getObject().position.y += moveSpeed * delta;
+        }
+        if (down)       {
+            controls.getObject().position.y -= moveSpeed * delta;
+        }
+    }
+
+    window.addEventListener("click", ()=>{
+        controls.lock();
+    });
+
+
+    window.addEventListener("keydown", (e) => {
+
+        switch (e.code) {
+            case "KeyW":
+                forward = true;     
+                break;
+            case "KeyS":
+                backward = true;    
+                break;
+            case "KeyA":
+                left = true;
+                break;
+            case "KeyD":
+                right = true;
+                break;
+            case "Space":
+                up = true;
+                break;
+            case "ControlLeft":
+                down = true;
+                break;
+        }
+
+    });
+
+    window.addEventListener("keyup", (e) => {
+        switch (e.code) {
+            case "KeyW":
+                forward = false;     
+                break;
+            case "KeyS":
+                backward = false;    
+                break;
+            case "KeyA":
+                left = false;
+                break;
+            case "KeyD":
+                right = false;
+                break;
+            case "Space":
+                up = false;
+                break;
+            case "ControlLeft":
+                down = false;
+                break;
+        }
+    });
+
+    return controls;
+}
+
+
 async function loadGallery() {
     const loader = new GLTFLoader();
     const galleryData = await loader.loadAsync("/assets/richard_art_gallery.glb");
@@ -29,22 +119,34 @@ async function loadGallery() {
 
 
 const scene = new THREE.Scene();
-scene.background = "purple";
+scene.background = new THREE.Color("skyblue");
 const camera = createCamera();
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+
+const controls = createControls(camera, renderer.domElement);
+
+
+
+
 const gallery = await loadGallery();
 scene.add(gallery);
 
 
-
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera);
+}
 
 function animate() {
     requestAnimationFrame(animate);
-
+    controls.update(0.01);
     renderer.render(scene, camera);
 }
 
