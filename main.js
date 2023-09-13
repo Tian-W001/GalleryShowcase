fe3abff6cb4c57de0c5369ce61c0752cddcf3838
Tian-W001 <wt1435831399@gmail.com>
2023-09-13 22:30:55 +1000

    camera can now switch targets smoothly

--- main.js
+++ main.js
@@ -7,11 +7,70 @@ import { PointerLockControls } from "three/examples/jsm/controls/PointerLockCont
 
 function createCamera() {
     const camera = new THREE.PerspectiveCamera(
-        35, 
+        40, 
         window.innerWidth / window.innerHeight, 
         0.1, 
         10000,
     );
+
+    //hardcoded camera positions and rotations
+    const positions = [
+        new THREE.Vector3(11.611884971474453,1.3727284997851807,-0.026261992950112414), 
+        new THREE.Vector3(10.43195522977472,1.6861324782133806,1.0781648947053117), 
+        new THREE.Vector3(12.621292281783422,1.6825220754993153,2.25690021947915), 
+    
+    ];
+    const rotations = [
+        new THREE.Quaternion(-0.02851451576759945,0.9684017199967849,0.11669640262690059,0.21855658469089476),
+        new THREE.Quaternion(-0.02126025156204034,0.8349585695759327,0.028380106969510065,0.5491691527422403),
+        new THREE.Quaternion(0.001705435887772891,-0.9987398311069736,-0.009496665953388767,0.04925093486033023)
+    ];
+
+
+    //the index of the current artwork
+    let index = 0;
+
+    camera.position.copy(positions[0]);
+    camera.setRotationFromQuaternion(rotations[0]);
+
+    window.addEventListener("keydown", (e)=>{
+        if (e.code == 'KeyN') {
+            if (index < positions.length - 1) {
+                index += 1;
+                console.log("Index: " + index);
+            }
+            else {
+                index = 0;
+            }
+
+        }
+        //Print current position and roatation for testing
+        else if (e.code == 'KeyC') {
+            console.log("new THREE.Vector3(" + 
+                camera.position.x + "," + 
+                camera.position.y + "," + 
+                camera.position.z + "), " + 
+                "\nnew THREE.Quaternion(" + 
+                camera.quaternion.x + "," + 
+                camera.quaternion.y + "," + 
+                camera.quaternion.z + "," + 
+                camera.quaternion.w + ")"
+            );
+        }
+    });
+
+    //update each frame
+    camera.update = (delta) => {
+        //Check if current position and rotation are inplace
+        if (camera.position == positions[index] && camera.rotation == rotations[index]){
+            return;
+        }
+
+        //lerp to desired transform
+        camera.position.lerp(positions[index], delta * 2);
+        camera.quaternion.slerp(rotations[index], delta * 2);
+    }
+
 
     return camera;
 }
@@ -116,7 +175,8 @@ async function loadGallery() {
     return gallery;
 }
 
-
+const clock = new THREE.Clock();
+clock.start();
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("skyblue");
@@ -145,9 +205,20 @@ function onWindowResize() {
 }
 
 function animate() {
+
+
     requestAnimationFrame(animate);
-    controls.update(0.01);
+
+
+    const delta = clock.getDelta();
+
+
+    camera.update(delta);
+    controls.update(delta);
+
     renderer.render(scene, camera);
+
+    clock.start();
 }
 
 animate();
