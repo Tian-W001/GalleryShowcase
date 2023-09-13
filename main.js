3ee193f8fb6df75027d18ad9891d2ffe0985d8ba
Tian-W001 <wt1435831399@gmail.com>
2023-09-13 21:26:23 +1000

    fix index.html and load gallery model

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
+
+
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+
+
+
+function createCamera() {
+    const camera = new THREE.PerspectiveCamera(
+        35, 
+        window.innerWidth / window.innerHeight, 
+        0.1, 
+        10000,
+    );
+
+    return camera;
+}
+
+async function loadGallery() {
+    const loader = new GLTFLoader();
+    const galleryData = await loader.loadAsync("/assets/richard_art_gallery.glb");
+    const gallery =  galleryData.scene;
+    gallery.position.set(0,0,0);
+    gallery.scale.set(1,1,1);
+    return gallery;
+}
+
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+scene.background = "purple";
+const camera = createCamera();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+const gallery = await loadGallery();
+scene.add(gallery);
+
+
+
 
 function animate() {
     requestAnimationFrame(animate);
@@ -15,3 +49,10 @@ function animate() {
 }
 
 animate();
+
+
+
+
+
+
+
